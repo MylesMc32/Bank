e2a4f37c25d526394d44ccbe3475dd2712058b34
Myles McKendell <Myles@myless-mbp>
2017-03-19 22:46:18 -0700

    Initial Commit

---
+++ Bank/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Bank
+//
+//  Created by Myles McKendell on 3/19/17.
+//  Copyright © 2017 Myles McKendell. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
